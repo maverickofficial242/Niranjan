11f5324f28d3f50126d6fbe4b4b5e44ee0bf43fe
MAVERICK <niranjan.502241@sxcce.edu.in>
2025-05-10 13:54:59 +0530

    Add files via upload

---
+++ Arithmetic/src/main/java/com/mycompany/arithmetic/Arithmetic.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.arithmetic;
+
+/**
+ *
+ * @author IT20
+ */
+public class Arithmetic {
+
+    public int add(int a,int b)
+    {
+        return a+b;
+    }
+    public int sub(int a,int b)
+    {
+        return a-b;
+    }
+    public int multiply(int a,int b)
+    {
+        return a*b;
+    }
+}
